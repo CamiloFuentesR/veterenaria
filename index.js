30c3d1d970a3d7c940f2cd377216b6c8eee41f23
CamiloFuentesR <lokomilo1@gmail.com>
2021-02-08 05:12:55 -0300

    form validtion

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ const corsOptions = {
     }
 }
 //con uso de cors
-//  app.use(cors(corsOptions));
+ app.use(cors(corsOptions));
 
 //sin cors limitado
-app.use(cors());
+// app.use(cors());
 
 //connectar a moongoDb
 mongoose.Promise = global.Promise;
