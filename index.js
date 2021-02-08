e05aae324ec0c913529c6a4b374971dc4af2963c
CamiloFuentesR <lokomilo1@gmail.com>
2021-02-08 06:28:30 -0300

    cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 
 //habilitar cors
-const whiteList = ['https://bddveterinaria.herokuapp.com']; //hace accesible solo desde esta url
+const whiteList = ['https://citasvete.netlify.app']; //hace accesible solo desde esta url
 const corsOptions = {
     origin: (origin, callbaback) => {
         //console.log(origin);
